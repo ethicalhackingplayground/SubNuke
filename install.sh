9b097eb1c61acdfc7c30d264984c641544a54c78
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2023-02-21 21:52:04 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,12 +34,23 @@ GOPATH=$HOME/go
 PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 git clone https://github.com/blechschmidt/massdns.git
 cd massdns ; make all ; cd ..
+
+wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
+tar -xvf assetfinder-linux-amd64-0.1.1.tgz
+mv assetfinder /bin ; chmod +x /bin/assetfinder
+
+curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux-i386.zip
+unzip findomain-linux-i386.zip
+chmod +x findomain
+sudo mv findomain /usr/bin/findomain
+
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/cgboal/sonarsearch/crobat@latest
 git clone https://github.com/jakejarvis/subtake.git
 go install -v github.com/jakejarvis/subtake@latest
 go install -v github.com/OWASP/Amass/v3/...@master
 go install -v github.com/j3ssie/metabigor@maste
+
 
 # Install the requirments
 pip3 install -r $(pwd)/../requirements.txt
